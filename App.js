49f0c1f5f35b3cb8962d68e11309d1e4c539f133
MOSA ISSA <mosasenio@gmail.com>
2023-02-09 19:08:10 +0200

    chenges
    some changes

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ const App = () => {
               I am vengeance
             </Text>
             <Text style={text.baseText}>
-              ??/??/????
+              ??/??/??
             </Text>
           </View>
 
